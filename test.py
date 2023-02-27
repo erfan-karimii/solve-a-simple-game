9dcd931f254a6259cf5fcc2c141309ed71f0e8c1
erfan <h410mi3@gmail.com>
2023-02-27 21:04:07 +0330

    fix test_treasure_place def

--- test.py
+++ test.py
@@ -9,23 +9,15 @@ class TestTrap(unittest.TestCase):
     def test_second_trap_place(self):
         self.assertNotEqual((trap_2_row,trap_2_element),(0,0))
         self.assertNotEqual((trap_2_row,trap_2_element),(trap_1_row,trap_1_element))
-        self.assertNotEqual((trap_2_row , trap_2_element ,trap_1_row , trap_1_element) , (1,0,0,1))
-        self.assertNotEqual((trap_2_row , trap_2_element ,trap_1_row , trap_1_element) , (0,1,1,0))
+        self.assertNotEqual((game_map[0][1],game_map[1][0]),('X','X'))
 
 
     def test_treasure_place(self):
-        if (trap_2_row , trap_2_element ,trap_1_row , trap_1_element) == (2,0,3,1):
-            self.assertEqual((treasure_row , treasure_element) , (3,0))
-        if (trap_2_row , trap_2_element ,trap_1_row , trap_1_element) == (0,2,1,3):
-            self.assertEqual((treasure_row , treasure_element) , (3,0))
-        if (trap_2_row , trap_2_element ,trap_1_row , trap_1_element) == (0,2,1,3):
-            self.assertEqual((treasure_row , treasure_element) , (0,3))
-        if (trap_2_row , trap_2_element ,trap_1_row , trap_1_element) == (1,3,0,2):
-            self.assertEqual((treasure_row , treasure_element) , (0,3))
-        if (trap_2_row , trap_2_element ,trap_1_row , trap_1_element) == (2,3,3,2):
-            self.assertEqual((treasure_row , treasure_element) , (3,3))
-        if (trap_2_row , trap_2_element ,trap_1_row , trap_1_element) == (3,2,2,3) :
-            self.assertEqual((treasure_row , treasure_element) , (3,3))
+        self.assertNotEqual((game_map[3][2],game_map[3][3],game_map[2][3]),('X','$','X'))
+        self.assertNotEqual((game_map[0][2],game_map[0][3],game_map[1][3]),('X','$','X'))
+        self.assertNotEqual((game_map[2][0],game_map[3][0],game_map[3][1]),('X','$','X'))
+
+
 
 
 
